a4da0d1c721564f12ef342bd1fce7d9afe7627ca
sania10rus-code <sania10rus@icloud.com>
2026-04-27 21:31:52 +0300

    Create parts catalog file

---
+++ src/lib/partsCatalog.ts
@@ -0,0 +1,36 @@
+// Parts Catalog
+
+const partsCatalog = {
+    Engine: [
+        { brand: 'Brand A', price: 500, stock: 'In stock' },
+        { brand: 'Brand B', price: 550, stock: 'In stock' },
+        { brand: 'Brand C', price: 600, stock: 'Out of stock' }
+    ],
+    Brakes: [
+        { brand: 'Brand D', price: 300, stock: 'In stock' },
+        { brand: 'Brand E', price: 320, stock: 'In stock' },
+        { brand: 'Brand F', price: 350, stock: 'In stock' }
+    ],
+    Suspension: [
+        { brand: 'Brand G', price: 400, stock: 'In stock' },
+        { brand: 'Brand H', price: 450, stock: 'Out of stock' },
+        { brand: 'Brand I', price: 475, stock: 'In stock' }
+    ],
+    Transmission: [
+        { brand: 'Brand J', price: 1200, stock: 'In stock' },
+        { brand: 'Brand K', price: 1250, stock: 'In stock' },
+        { brand: 'Brand L', price: 1300, stock: 'Out of stock' }
+    ],
+    Cooling: [
+        { brand: 'Brand M', price: 200, stock: 'In stock' },
+        { brand: 'Brand N', price: 220, stock: 'In stock' },
+        { brand: 'Brand O', price: 230, stock: 'Out of stock' }
+    ],
+    Exhaust: [
+        { brand: 'Brand P', price: 700, stock: 'In stock' },
+        { brand: 'Brand Q', price: 750, stock: 'In stock' },
+        { brand: 'Brand R', price: 800, stock: 'Out of stock' }
+    ]
+};
+
+export default partsCatalog;
